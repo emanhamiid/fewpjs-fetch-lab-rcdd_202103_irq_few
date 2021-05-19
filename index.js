0382f14be1d31b348d647b0516527a8fd87e3378
eman hamid <emanhharki@gmail.com>
2021-05-19 21:38:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-
-
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
+}
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector('main')
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const h2 = document.createElement('h2')
+    h2.innerHTML = book.name
+    main.appendChild(h2)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  function fetchBooks() {
-    // To pass the tests, don't forget to return your fetch!
-    const URLs = 'https://anapioficeandfire.com/api/books';
-    fetch(URLs)
-      .then(resp => resp.json())
-      .then(json => console.log(json));
-
-  }
-  fetchBooks();
-});
+  fetchBooks()
+})
